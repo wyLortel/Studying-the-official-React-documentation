eb423fb5c856ecb71471e88e3e1fc2ebcd9ae3c8
JEOUNG WOOYOUNG <young99@g.yju.ac.kr>
2025-10-26 20:53:00 +0900

    2025-10-26

---
+++ 05-starting-project/src/components/ProjectsSidebar.jsx
@@ -0,0 +1,15 @@
+export default function ProjectsSidebar(){
+    return(
+        // 유데미 148 2분까지 테일윈드 뿌수고옴 뭔말인지 하나도 모르겟음
+        <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
+            <h2>Your Projects</h2>
+            <div>
+                <button>
+                   + Add Project
+                </button>
+            </div>
+            <ul>  
+            </ul>
+        </aside>
+    )
+}
